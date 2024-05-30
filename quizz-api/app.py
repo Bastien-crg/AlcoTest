b1f4ea7db531fec1c4746ff464286e2bed3ad03a
yannlbh <yann.lebihan@edu.esiee.fr>
2024-05-30 15:19:20 +0200

    Login request

--- quizz-api/app.py
+++ quizz-api/app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+import jwt_utils
 
 app = Flask(__name__)
 app = Flask(__name__)
@@ -13,6 +14,17 @@ def hello_world():
 @app.route('/quiz-info', methods=['GET'])
 def GetQuizInfo():
 	return {"size": 0, "scores": []}, 200
+
+
+@app.route('/login', methods=['POST'])
+def Login():
+	payload = request.get_json()
+	passwordUser = payload["password"]
+	if(passwordUser == "flask2023"):
+		token = jwt_utils.build_token()
+		return jsonify({'token': token}), 200
+	return 'Unauthorized', 401
+
 
 if __name__ == "__main__":
     app.run()
